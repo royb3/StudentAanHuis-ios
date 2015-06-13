f5b484df1dfe186b03518ce783d8fbd5440bfc35
Roy Buitenhuis <roy.buitenhuis94@gmail.com>
2015-06-13 23:26:57 +0200

    Initial Commit

---
+++ StudentAanHuis/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  StudentAanHuis
+//
+//  Created by Roy Buitenhuis on 13-06-15.
+//  Copyright (c) 2015 Roy Buitenhuis. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
